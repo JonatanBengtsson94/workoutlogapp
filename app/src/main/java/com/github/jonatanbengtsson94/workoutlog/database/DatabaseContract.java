594bf326fe923edb4359ff7b7e6a5b76a902807f
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-09-28 15:10:18 +0200

    started work on database

---
+++ app/src/main/java/com/github/jonatanbengtsson94/workoutlog/database/DatabaseContract.java
@@ -0,0 +1,28 @@
+package com.github.jonatanbengtsson94.workoutlog.database;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+
+    private DatabaseContract() {}
+
+    public static class SetsTable implements BaseColumns {
+        public static final String TABLE_NAME = "sets";
+        public static final String COLUMN_SET_ID = "set_id";
+        public static final String COLUMN_REPS = "reps";
+        public static final String COLUMN_WEIGHT = "weight";
+        public static final String COLUMN_EXERCISE_ID_FK = "exercise_id_fk";
+    }
+
+    public static class ExercisesTable implements BaseColumns {
+        public static final String TABLE_NAME = "exercises";
+        public static final String COLUMN_EXERCISE_ID = "exercise_id";
+        public static final String COLUMN_EXERCISE_NAME = "exercise_name";
+        public static final String COLUMN_WORKOUT_ID_FK = "workout_id_fk";
+    }
+
+    public static class WorkoutTable implements BaseColumns {
+        public static final String TABLE_NAME = "workouts";
+        public static final String COLUMN_DATE_PERFORMED = "date_performed";
+    }
+}
