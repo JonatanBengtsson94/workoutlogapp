fcfacd2e75df242451d7314a22a8c2269939b85b
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-09-29 21:02:31 +0200

    workouts now have a name

--- app/src/main/java/com/github/jonatanbengtsson94/workoutlog/model/Workout.java
+++ app/src/main/java/com/github/jonatanbengtsson94/workoutlog/model/Workout.java
@@ -1,7 +1,10 @@
 package com.github.jonatanbengtsson94.workoutlog.model;
 
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class Workout {
+    private String name;
+    private LocalDate datePerformed;
     private ArrayList<Exercise> exercises;
 }
