e320fbc73c31b1dca8e1cf4f7b59e534c84fb33d
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-11-15 18:41:47 +0100

    added perform again button

--- app/src/main/java/com/jonatanbengtsson/workoutlog/ui/fragments/WorkoutDetailFragment.java
+++ app/src/main/java/com/jonatanbengtsson/workoutlog/ui/fragments/WorkoutDetailFragment.java
@@ -1,22 +1,28 @@
 package com.jonatanbengtsson.workoutlog.ui.fragments;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.jonatanbengtsson.workoutlog.R;
 import com.jonatanbengtsson.workoutlog.model.Workout;
+import com.jonatanbengtsson.workoutlog.ui.activities.CreateEmptyWorkoutActivity;
 import com.jonatanbengtsson.workoutlog.ui.adapters.ExercisePerformedAdapter;
 
 public class WorkoutDetailFragment extends DialogFragment {
     private Workout workout;
     private RecyclerView recyclerView;
+    private Button btnPerformAgain;
 
     public static WorkoutDetailFragment newInstance(Workout workout) {
         WorkoutDetailFragment fragment = new WorkoutDetailFragment();
@@ -36,6 +42,13 @@ public class WorkoutDetailFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_workout_detail, container, false);
+
+        btnPerformAgain = rootView.findViewById(R.id.btnPerformAgain);
+        btnPerformAgain.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), CreateEmptyWorkoutActivity.class);
+            startActivity(intent);
+        });
+
         recyclerView = rootView.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
@@ -45,7 +58,6 @@ public class WorkoutDetailFragment extends DialogFragment {
         }, error -> {
             Toast.makeText(getContext(), "Database error", Toast.LENGTH_SHORT).show();
         });
-
 
         return rootView;
     }
